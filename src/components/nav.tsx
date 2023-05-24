33b58828dcea1b036dd9ed15aa2e50bebee91108
Johann Mendoza <johannmendoza1209@gmail.com>
2023-05-24 17:39:07 +0800

    nav comp and changing theme

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <div className="flex bg-slate-200 dark:bg-zinc-900 dark:text-white px-6 py-4">
       <div className="flex w-full justify-between items-center">
-        <h1 className="">Job Listings</h1>
+        <h1 className="font-bold">Job Listings</h1>
         <Searchbar />
         <NavButtons />
       </div>
--- src/components/navcomponents.tsx
+++ src/components/navcomponents.tsx
@@ -10,7 +10,7 @@ import {
 
 function Searchbar() {
   return (
-    <div className="flex gap-4 items-center">
+    <div className="hidden sm:flex gap-4 items-center">
       <MagnifyingGlassIcon className="h-5 w-5" />
       <input
         type="text"
@@ -26,9 +26,9 @@ function NavButtons() {
 
   return (
     <div className="flex items-center gap-4 ">
-      <div className="flex gap-2 items-center cursor-pointer border-2 border-black py-1 px-2 dark:border-white">
+      <div className="flex gap-2 items-center cursor-pointer sm:border-2 border-black py-1 px-2 dark:border-white">
         <PlusIcon className="h-5 w-5" />
-        <h1 className="shrink-0">New Job</h1>
+        <h1 className="hidden sm:block shrink-0">New Job</h1>
       </div>
       <div
         className="flex cursor-pointer"
