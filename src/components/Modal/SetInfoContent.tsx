05fe7f8002c3c33822d3210b4f424e7a273233ef
Johann Mendoza <johannmendoza1209@gmail.com>
2023-06-20 17:33:51 +0800

    close on submit, and error handling bug fix

--- src/components/Modal/SetInfoContent.tsx
+++ src/components/Modal/SetInfoContent.tsx
@@ -2,10 +2,10 @@
 
 import {
   JobType,
-  newNote,
-  updateNote,
   getNote,
+  newNote,
   statusTypes,
+  updateNote,
 } from "@/utils/dbActions";
 import useModalState from "@/utils/modalState";
 import optionValues from "@/utils/optionValues";
@@ -14,15 +14,10 @@ import { AnimatePresence, motion } from "framer-motion";
 import moment from "moment";
 import { useSession } from "next-auth/react";
 import { useEffect, useState, useTransition } from "react";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
 
 export default function SetInfoContent() {
-  const {
-    register,
-    setValue,
-    handleSubmit,
-    formState: { errors, dirtyFields },
-  } = useForm<JobType>({
+  const methods = useForm<JobType>({
     defaultValues: {
       title: "",
       dateOfApplication: moment().toDate(),
@@ -33,13 +28,20 @@ export default function SetInfoContent() {
       status: "APPLYING",
     },
   });
+
+  const {
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors, dirtyFields },
+  } = methods;
+
   const [isPending, startTransition] = useTransition();
   const [isOptionalOpen, setIsOptionalOpen] = useState(false);
   const jobID = useModalState((state) => state.modalID);
   const modalContent = useModalState((state) => state.modalContent);
+  const closeModal = useModalState((state) => state.closeModal);
   const session = useSession();
-  const emailRegex =
-    /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?/g;
 
   const requiredFormInputs = [
     ["title", "Title"],
@@ -57,7 +59,7 @@ export default function SetInfoContent() {
     const { companyAddress, description, link } = data;
     startTransition(() => {
       if (modalContent === "updateInfo") {
-        updateNote(
+        const isSuccessful = updateNote(
           {
             ...data,
             companyAddress:
@@ -71,7 +73,7 @@ export default function SetInfoContent() {
           jobID
         );
       } else {
-        newNote(
+        const isSuccessful = newNote(
           {
             ...data,
             companyAddress:
@@ -86,6 +88,7 @@ export default function SetInfoContent() {
           session.data?.user?.email!
         );
       }
+      closeModal();
     });
   });
 
@@ -124,149 +127,166 @@ export default function SetInfoContent() {
         {modalContent === "updateInfo" && "Update "}
         Job Application
       </h1>
-      <form
-        onSubmit={onSubmit}
-        className="flex flex-col gap-2"
-      >
-        <div className="flex flex-col gap-2">
-          {requiredFormInputs.map(([value, label], index) => (
-            <div
-              className="flex flex-col"
-              key={index}
-            >
-              <div className="flex justify-between">
-                <label className="capitalize opacity-90">{label}*</label>
-                {errors[value as keyof JobType] && (
-                  <p className="text-red-500">
-                    {errors[value as keyof JobType]?.message as React.ReactNode}
-                  </p>
-                )}
-              </div>
-              <input
-                type={value === "dateOfApplication" ? "date" : "text"}
-                className="border-2 border-black bg-transparent p-2 outline-blue-400 dark:border-white"
-                required
-                {...register(value as any, {
-                  required: "This field is required!",
-                })}
-              />
-            </div>
-          ))}
-          <div className="flex flex-col">
-            <div className="flex justify-between">
-              <label className="capitalize opacity-90">Status*</label>
-
-              {errors.status && (
+      <FormProvider {...methods}>
+        <form
+          onSubmit={onSubmit}
+          className="flex flex-col gap-2"
+        >
+          <div className="flex flex-col gap-2">
+            <FormInputGroup
+              data={requiredFormInputs}
+              isRequired="This field is required"
+            />
+            <div className="flex flex-col">
+              <div className="flex items-end justify-between">
+                <label className="capitalize opacity-90">Status*</label>
                 <p className="text-red-500">{errors?.status?.message}</p>
-              )}
-            </div>
+              </div>
 
-            <select
-              className="border-2 border-black bg-transparent p-2 outline-blue-400 dark:border-white"
-              defaultValue="APPLYING"
-              onChange={(e) =>
-                setValue("status", e.currentTarget.value as statusTypes)
-              }
-            >
-              {Object.keys(optionValues).map((value, index) => (
-                <option
-                  className="text-black"
-                  value={value}
-                  key={index}
-                >
-                  {optionValues[value as keyof typeof optionValues]}
-                </option>
-              ))}
-            </select>
-          </div>
-          <AnimatePresence initial={false}>
-            {isOptionalOpen && (
-              <motion.div
-                className="flex flex-col gap-2"
-                initial="hidden"
-                animate="open"
-                exit="hidden"
-                variants={{
-                  open: {
-                    height: "auto",
-                    transition: {
-                      delayChildren: 0.15,
-                    },
-                  },
-                  hidden: {
-                    height: 0,
-                    transition: {
-                      delay: 0.15,
-                    },
-                  },
-                }}
-                transition={{
-                  duration: 1,
-                  ease: "easeInOut",
-                }}
+              <select
+                className="border-2 border-black bg-transparent p-2 outline-blue-400 dark:border-white"
+                defaultValue="APPLYING"
+                onChange={(e) =>
+                  setValue("status", e.currentTarget.value as statusTypes)
+                }
               >
-                {optionalFormInputs.map(([value, label], index) => (
+                {Object.keys(optionValues).map((value, index) => (
+                  <option
+                    className="text-black"
+                    value={value}
+                    key={index}
+                  >
+                    {optionValues[value as keyof typeof optionValues]}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <AnimatePresence initial={false}>
+              {isOptionalOpen && (
+                <motion.div
+                  className="flex flex-col gap-2"
+                  initial="hidden"
+                  animate="open"
+                  exit="hidden"
+                  variants={{
+                    open: {
+                      height: "auto",
+                      transition: {
+                        delayChildren: 0.18,
+                      },
+                    },
+                    hidden: {
+                      height: 0,
+                      transition: {
+                        delay: 0.15,
+                      },
+                    },
+                  }}
+                  transition={{
+                    duration: 1,
+                    ease: "easeInOut",
+                  }}
+                >
                   <motion.div
                     variants={{
                       hidden: { opacity: 0, y: -20 },
                       open: { opacity: 1, y: 0 },
                     }}
                     className="flex flex-col"
-                    key={index}
                   >
-                    <div className="flex justify-between">
-                      <label className="capitalize opacity-90">{label}</label>
-                      {errors[value as keyof JobType] && (
-                        <p className="text-red-500">
-                          {
-                            errors[value as keyof JobType]
-                              ?.message as React.ReactNode
-                          }
-                        </p>
-                      )}
-                    </div>
-
-                    <input
-                      className="border-2 border-black bg-transparent p-2 outline-blue-400 dark:border-white"
-                      {...register(value as any, {
-                        pattern: value === "link" ? emailRegex : undefined,
-                      })}
-                    />
+                    <FormInputGroup data={optionalFormInputs} />
                   </motion.div>
-                ))}
-              </motion.div>
-            )}
-          </AnimatePresence>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            <button
+              type="button"
+              className="flex"
+              onClick={() => setIsOptionalOpen((bool) => !bool)}
+            >
+              {isOptionalOpen ? (
+                <div className="flex items-center gap-4">
+                  <ChevronUpIcon className="h-5 w-5" />
+                  <h1 className="text-sm">Hide Optional Info</h1>
+                </div>
+              ) : (
+                <div className="flex items-center gap-4">
+                  <ChevronDownIcon className="h-5 w-5" />
+                  <h1 className="text-sm">Show Optional Info</h1>
+                </div>
+              )}
+            </button>
+          </div>
           <button
-            type="button"
-            className="flex"
-            onClick={() => setIsOptionalOpen((bool) => !bool)}
+            className=" button_transition self-end bg-blue-400 text-white enabled:hover:bg-blue-500 disabled:bg-blue-200"
+            disabled={isPending}
+            type="submit"
           >
-            {isOptionalOpen ? (
-              <div className="flex items-center gap-4">
-                <ChevronUpIcon className="h-5 w-5" />
-                <h1 className="text-sm">Hide Optional Info</h1>
-              </div>
-            ) : (
-              <div className="flex items-center gap-4">
-                <ChevronDownIcon className="h-5 w-5" />
-                <h1 className="text-sm">Show Optional Info</h1>
-              </div>
-            )}
+            {isPending
+              ? "Loading..."
+              : modalContent === "updateInfo"
+              ? "Update"
+              : "Submit"}
           </button>
-        </div>
-        <button
-          className=" button_transition self-end bg-blue-400 text-white enabled:hover:bg-blue-500 disabled:bg-blue-200"
-          disabled={isPending}
-          type="submit"
-        >
-          {isPending
-            ? "Loading..."
-            : modalContent === "updateInfo"
-            ? "Update"
-            : "Submit"}
-        </button>
-      </form>
+        </form>
+      </FormProvider>
     </div>
+  );
+}
+
+function FormInputGroup({
+  data,
+  isRequired,
+}: {
+  data: string[][];
+  isRequired?: string;
+}) {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext();
+
+  const emailRegex =
+    /(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?/g;
+
+  return (
+    <>
+      {data.map(([value, label], index) => (
+        <div
+          className="flex flex-col"
+          key={index}
+        >
+          <div className="flex items-end justify-between">
+            <label className="capitalize opacity-90">
+              {label}
+              {isRequired && "*"}
+            </label>
+            <p className="text-sm text-red-500">
+              {errors[value as keyof JobType]?.message as React.ReactNode}
+            </p>
+          </div>
+
+          <input
+            className="border-2 border-black bg-transparent p-2 outline-blue-400 dark:border-white"
+            type={value === "dateOfApplication" ? "date" : "text"}
+            required={isRequired !== undefined}
+            {...register(value as any, {
+              pattern:
+                value === "link"
+                  ? {
+                      value: emailRegex,
+                      message: "Value is not accepted as link",
+                    }
+                  : undefined,
+              required: {
+                value: isRequired !== undefined,
+                message: isRequired ?? "",
+              },
+            })}
+          />
+        </div>
+      ))}
+    </>
   );
 }
