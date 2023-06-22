00d360eeacb7d8a6ead6ca24a219bd2d077fa9c3
Johann Mendoza <johannmendoza1209@gmail.com>
2023-06-22 13:13:55 +0800

    added github auth provider

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,15 +1,20 @@
 import { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import GitHubProvider from "next-auth/providers/github";
 
 const authOptions: NextAuthOptions = {
-   providers: [
+  providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string, 
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string
+    })
   ],
   pages: {
-       signIn: "/signin"
+    signIn: "/signin"
   },
 }
 
